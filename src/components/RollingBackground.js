50897fc92c352fcc733e29f664c1b60ced415f9d
burvxs <benny.purvis@hotmail.com>
2020-08-27 10:03:38 +1000

    Added Fragment to RollingBG

--- src/components/RollingBackground.js
+++ src/components/RollingBackground.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 class RollingBackground extends Component {
     render() {
         return (
-          <div>
+          <React.Fragment>
             <div className="bg-image img1"></div>
             <div className="bg-image img2"></div>
             <div className="bg-image img3"></div>
             <div className="bg-image img4"></div>
             <div className="bg-image img5"></div>
             <div className="bg-image img6"></div>
-          </div>
+          </React.Fragment>
         );
     }
 }
